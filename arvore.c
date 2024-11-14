e87340aabb20abb15ae357e8a7e1ac533a07fd01
Marcela Silva <marcela013sb@hotmail.com>
2024-11-14 17:07:14 -0300

    Update arvore.c

--- arvore.c
+++ arvore.c
@@ -1,3 +1,4 @@
+
 #include "arvore.h"
 
 
@@ -11,9 +12,15 @@ No* inicializar_arvore(){
     return n;
 }
 
-
-
-
+void exibir_arvore(No* raiz){
+    printf("\n< %d (Título: %s, Autor: %s)", raiz->livro->codigo, raiz->livro->titulo);
+    {
+    //printf("\n< %d (Título: %s, Autor: %s, Gênero: %s, Ano: %d, Editora: %s, Nº Páginas: %d)", raiz->livro->codigo, raiz->livro->titulo, raiz->livro->autor, raiz->livro->genero, raiz->livro->ano, raiz->livro->editora, raiz->livro->numeroPaginas);
+    }
+    exibir_arvore(raiz->sae);
+    exibir_arvore(raiz->sad);
+    printf(">");
+}
 
 // libera a memória dos nós e livros a começar dos níveis mais baixos
 void liberar_arvore(No* raiz){
